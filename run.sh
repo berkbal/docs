fc8259eec6e65ece487c68a7855cc488c1517409
berkbal <berkbal94@gmail.com>
2024-10-26 23:24:08 +0300

    Kullanilmayan menulerin kaldirilmasi

--- run.sh
+++ run.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-
-# Sanal ortam oluşturma
 python3 -m venv venv
-
-# Gereksinimleri yükleme (sanal ortamı etkinleştirerek)
-source venv/bin/activate && pip install -r requirements.txt
-
-echo "Sanal ortam oluşturuldu ve gereksinimler yüklendi."
-echo "Sanal ortamı aktif etmek için 'source venv/bin/activate' komutunu kullanın."
+source venv/bin/activate
+pip install -r requirements.txt
